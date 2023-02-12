dc0115d898f3d5cc3529565d5aa6cb4e9d49d492
Jagadish2023 <125132009+Jagadish2023@users.noreply.github.com>
2023-02-12 15:17:19 +0530

    Create second.py
    simple Python program that calculates the area of a circle

---
+++ second.py
@@ -0,0 +1,6 @@
+#simple Python program that calculates the area of a circle:
+import math
+
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * radius**2
+print("The area of the circle is", area)
